5eb279fe308c094801190757a2e15119315ed0bb
Laurent0s <d.nikitin.j@gmail.com>
2025-08-10 00:19:25 +0300

    feat.SCRUM-15.Api integration

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,14 @@
-// next.config.ts
-import type { NextConfig } from "next";
-
 const isDev = process.env.NODE_ENV === "development";
-// Deploy
-const nextConfig: NextConfig = {
+
+const nextConfig = {
   async rewrites() {
-    return [
-      { source: "/api/price-analyze/:path*", destination: "http://46.62.152.2/price-analyze/:path*" },
-      { source: "/api/price-years/:path*",  destination: "http://46.62.152.2/price-years/:path*" },
-      { source: "/api/price-budget/:path*", destination: "http://46.62.152.2/price-budget/:path*" },
-    ];
+    return isDev
+      ? [
+          { source: "/api/price-analyze/:path*", destination: "http://46.62.152.2/price-analyze/:path*" },
+          { source: "/api/price-years/:path*",   destination: "http://46.62.152.2/price-years/:path*" },
+          { source: "/api/price-budget/:path*",  destination: "http://46.62.152.2/price-budget/:path*" },
+        ]
+      : [];
   },
 };
-
 export default nextConfig;
